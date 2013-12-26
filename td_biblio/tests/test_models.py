8e21fa6b00cf0fc2da9f2d28d3ca6777db83ef8a
Julien Maupetit <julien@maupetit.net>
2013-12-26 20:53:33 +0100

    Add a test for django user/author linking

--- td_biblio/tests/test_models.py
+++ td_biblio/tests/test_models.py
@@ -4,6 +4,7 @@ TailorDev Bibliography
 
 Test models.
 """
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 from ..factories import (AuthorFactory, EditorFactory, JournalFactory,
@@ -77,6 +78,16 @@ class AbstractHumanModelTestMixin(ModelTestMixin):
         formatted_name = human.get_formatted_name()
         expected = "McClane J"
         self.assertEqual(formatted_name, expected)
+
+    def test_user_linking(self):
+        """
+        Test linking the AbstractHuman object with a django user
+        """
+        user = get_user_model().objects.create(username='johnmcclane')
+        human = self.factory(first_name="John",
+                             last_name="McClane",
+                             user=user)
+        self.assertEqual(human.user, user)
 
 
 class AuthorModelTest(AbstractHumanModelTestMixin, TestCase):
