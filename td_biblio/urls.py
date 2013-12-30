4cb3aff6fd2de0ee6406a4cc6f0c871193d63cb8
Julien Maupetit <julien@maupetit.net>
2013-12-30 13:41:26 +0100

    Add entry list basic view

---
+++ td_biblio/urls.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import url, patterns
+
+from .views import EntryListView
+
+
+urlpatterns = patterns('',
+
+    # Entry List
+    url('^$', EntryListView.as_view(), name='entry_list'),
+)
--- td_biblio/views.py
+++ td_biblio/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+from django.views.generic import ListView
+
+from .models import Entry
+
+
+class EntryListView(ListView):
+    """Entry list view"""
+    model = Entry
+    template = 'td_biblio/entry_list.html'
