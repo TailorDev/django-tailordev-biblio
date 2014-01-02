c80b1400e8fb685a983a87340b7c4cbbe6f91eec
Julien Maupetit <julien@maupetit.net>
2014-01-02 22:36:37 +0100

    Fix pep8

--- td_biblio/urls.py
+++ td_biblio/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls import url, patterns
 from .views import EntryListView
 
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
 
     # Entry List
     url('^$', EntryListView.as_view(), name='entry_list'),
