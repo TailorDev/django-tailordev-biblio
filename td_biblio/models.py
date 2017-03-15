8d613072e3f73c0ebea093140a975fd72249e6ae
Julien Maupetit <julien@tailordev.fr>
2017-03-15 11:16:15 +0100

    Lazy load gettext

--- td_biblio/models.py
+++ td_biblio/models.py
@@ -2,7 +2,7 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db import models
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
 
 class AbstractHuman(models.Model):
