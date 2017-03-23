8bb6e9fec4ff942951e319f7abdca21c874773c7
Julien Maupetit <julien@tailordev.fr>
2017-03-23 18:11:32 +0100

    Use a logger to maintain Django>=1.7,<1.9 compat.

--- td_biblio/management/commands/bibtex_import.py
+++ td_biblio/management/commands/bibtex_import.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import logging
+
 from django.core.management.base import BaseCommand, CommandError
 
 from td_biblio.utils.managers import bibtex_import
@@ -7,6 +9,10 @@ from td_biblio.utils.managers import bibtex_import
 class Command(BaseCommand):
     help = "Import entries from a BibTex file"
 
+    def _get_logger(self):
+        logger = logging.getLogger('td_biblio')
+        return logger
+
     def add_arguments(self, parser):
         parser.add_argument(
             'bibtex',
@@ -14,10 +20,12 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
+        logger = self._get_logger()
+
         bibtex = options.get('bibtex', None)
         if bibtex is None:
             raise CommandError("A BibTeX file path is required")
 
-        self.stdout.write("Importing '{}' BibTeX file...".format(bibtex))
+        logger.info("Importing '{}' BibTeX file...".format(bibtex))
         bibtex_import(bibtex)
-        self.stdout.write(self.style.SUCCESS("Importation succeeded"))
+        logger.info("Importation succeeded")
