94c0a470f26030980aee3405c78646122196c191
Julien Maupetit <julien@tailordev.fr>
2017-06-09 17:41:00 +0200

    Update sandbox configuration for Heroku

--- sandbox/wsgi.py
+++ sandbox/wsgi.py
@@ -1,16 +1,11 @@
 """
-WSGI config for toto project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+WSGI config for td_biblio sandbox.
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sandbox.settings')
 
 application = get_wsgi_application()
