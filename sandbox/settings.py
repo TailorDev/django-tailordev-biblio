645a8406e4955c239b8642f7fbb33403e415cec4
Julien Maupetit <julien@tailordev.fr>
2017-06-09 22:50:51 +0200

    Inactivate the whitenoise STATICFILES_STORAGE
    As stated in the documentation, this storage may lead to failures for CSS files
    mentionning extra files.
    http://whitenoise.evans.io/en/stable/django.html#troubleshooting-the-whitenoise-storage-backend

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -119,4 +119,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
